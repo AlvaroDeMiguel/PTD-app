8a8bb0c0bafc74b50f5647656a93c8fa9a118dcf
AlvaroDeMiguel <alvar.demi@gmail.com>
2025-03-11 12:06:21 +0100

    Commit - colores añadidos y comiendo del componente textfield

--- app/src/main/java/com/example/ptdapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/ptdapp/ui/theme/Color.kt
@@ -5,7 +5,13 @@ import androidx.compose.ui.graphics.Color
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Green = Color(0xFF00BF63) // #00bf63
+val BlueLight = Color(0xFF38B6FF) // #38b6ff
+val Gray = Color(0xFF5F6368) // #5f6368
+val LightBlue = Color(0xFFA7D8F5) // #a7d8f5
+val LightGray = Color(0xFFEFefEF) // #efefef
+val LightBlueDark = Color(0xFF8CC7E8)
--- app/src/main/java/com/example/ptdapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/ptdapp/ui/theme/Theme.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
@@ -32,6 +33,16 @@ private val LightColorScheme = lightColorScheme(
     onSurface = Color(0xFF1C1B1F),
     */
 )
+private val CustomLightColorScheme = lightColorScheme(
+    primary = BlueLight, // Botones
+    onPrimary = Color.White,
+    secondary = Green,
+    onSecondary = Color.White,
+    background = LightGray, // Fondo general
+    onBackground = Gray, // Texto sobre fondo general
+    surface = LightBlue, // TextField
+    onSurface = Gray // Texto sobre TextField
+)
 
 @Composable
 fun PTDAppTheme(
@@ -47,7 +58,7 @@ fun PTDAppTheme(
         }
 
         darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        else -> CustomLightColorScheme
     }
 
     MaterialTheme(
